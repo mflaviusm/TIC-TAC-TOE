d38d3f5a74c9dd506c69d775212d191365a187b1
mflaviusm <flaviusmiler@yahoo.co.uk>
2021-09-14 20:18:52 +0000

    Create display_board function to show board with friendly UX

--- run.py
+++ run.py
@@ -1,4 +1,21 @@
 #Welcome to the TIC-TAC-TOE game!
 
 board = [' ' for x in range(10)]
-print(board)
+
+def display_board(board):
+    """
+    Displays the board in a more styles manner
+    """
+    print('      |     |')
+    print('   ' + board[1] + '  |  ' + board[2] + '  |  ' + board[3])
+    print('      |     |')
+    print('-------------------')
+    print('      |     |')
+    print('   ' + board[4] + '  |  ' + board[5] + '  |  ' + board[6])
+    print('      |     |')
+    print('-------------------')
+    print('      |     |')
+    print('   ' + board[7] + '  |  ' + board[8] + '  |  ' + board[9])
+    print('      |     |')
+
+display_board(board)
