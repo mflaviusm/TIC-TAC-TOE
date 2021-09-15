54453857c19862c3bf152bcfaeb4c497986e8c18
mflaviusm <flaviusmiler@yahoo.co.uk>
2021-09-15 15:09:00 +0000

    Add docstrings to functions

--- run.py
+++ run.py
@@ -1,19 +1,30 @@
 # Welcome to the TIC-TAC-TOE game!
 
 board = [' ' for x in range(10)]
+"""
+The board is list accessed using a for loop.
+"""
 
 
 def check_move(letter, position):
+    """
+    This function is an auxiliary function for player_move
+    function that will insert the player chosen position onto the board.
+    """
     board[position] = letter
 
 
 def available_space(position):
+    """
+    This function is an auxiliary function for player_move that will
+    check that the user choice can be inserted onto the chosen location.
+    """
     return board[position] == ' '
 
 
 def display_board(board):
     """
-    Displays the board in a more styles manner
+    Displays the board to the user using print statements
     """
     print('      |     |')
     print('   ' + board[1] + '  |  ' + board[2] + '  |  ' + board[3])
@@ -29,10 +40,18 @@ def display_board(board):
 
 
 def winning_combs(brd, let):
+    """
+    This function checks for any possible combinations to ensure
+    that if either the user or the ai get 3 in a line, the game ends.
+    """
     return (brd[7] == let and brd[8] == let and brd[9] == let) or (brd[4] == let and brd[5] == let and brd[6] == let) or (brd[1] == let and brd[2] == let and brd[3] == let) or (brd[1] == let and brd[4] == let and brd[7] == let) or (brd[2] == let and brd[5] == let and brd[8] == let) or (brd[3] == let and brd[6] == let and brd[9] == let) or (brd[1] == let and brd[5] == let and brd[9] == let) or (brd[3] == let and brd[5] == let and brd[7] == let)
 
 
 def board_spaces(board):
+    """
+    This function checks if there are any empty
+    spaces remaining on the board
+    """
     if board.count(' ') > 1:
         return False
     else:
@@ -40,6 +59,11 @@ def board_spaces(board):
 
 
 def player_move():
+    """
+    This function record the player's input,
+    converts it into an integer and then validates it to ensure the user
+    inputs a number form 1 to 9, using a while loop and a try-except statement.
+    """
     execute = True
     while execute:
         user_input = input('Please select a number ranging from 1 to 9 to place the \'X\':\n')
@@ -58,6 +82,10 @@ def player_move():
 
 
 def random_choice(list):
+    """
+    This in an auxiliary function for the ai_move function
+    that will generate a random number using the python ramdom library.
+    """
     import random
     length = len(list)
     range = random.randrange(0, length)
@@ -65,6 +93,13 @@ def random_choice(list):
 
 
 def ai_move():
+    """
+    This function makes the move for the computer.
+    First it will check if the computer can do a winning move,
+    if not it will check to see if the player can make a winning move.
+    If not it will check for available spaces in the corners,
+    then the sides and lastly the middle.
+    """
     possible_moves = [x for x, letter in enumerate(board) if letter == ' ' and x != 0]
     move = 0
 
@@ -101,6 +136,11 @@ def ai_move():
 
 
 def main():
+    """
+    The main function prints the board and the welcome message,
+    and uses a while loop to check for empty spaces and winning moves,
+    if the game is a tie, if not the player and ai will take another turn.
+    """
     print('Welcome to the TIC-TAC-TOE game!')
     display_board(board)
 
@@ -127,6 +167,10 @@ def main():
 
 main()
 
+
+"""
+This while loop is used to check if the user would like to play another game.
+"""
 while True:
     user_reply = input('Would you like to have another go? (Y/N):\n')
     if user_reply.lower() == 'y' or user_reply.lower() == 'yes':
